daaf70663b3d70202c56630f3a6bb78d6fbe2b2b
kzmbo <65250474+kzmbo@users.noreply.github.com>
2021-09-07 18:38:31 -0700

    added a continue prompt

--- src/displayAllExpenses.java
+++ src/displayAllExpenses.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class displayAllExpenses {
     public static void main(String[] arg){
-
+    	Scanner in = new Scanner( System.in );
+    	recordExpense expenseL = new recordExpense();
+    	expenseL.expensesList.display(true);
+    	linkedList.Node currentE = expenseL.expensesList.head;
+    	double totExp = 0;
+    	while(currentE != null) {    
+            //Calculates total expenses.  
+            totExp = totExp + currentE.money;    
+            currentE = currentE.next;    
+        }
+    	System.out.println("Your total expenses are: $" + totExp);
+		System.out.println("Hit Enter to continue...");
+		if(in.hasNextLine()) {
+			lab2.askPrompt(in);
+			System.exit(0);
+		}
+    	System.exit(0);
     }
 }
